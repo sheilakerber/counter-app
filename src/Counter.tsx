5ceeea95845b4ef682bca9c38e4a3f9881bd3f0b
Sheila Kerber <sheila.skerber@gmail.com>
2021-12-17 14:23:17 -0300

    shows in the page how many times the value was changed by the user using useEffect

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export function Counter(){
 
@@ -26,13 +26,24 @@ export function Counter(){
     function resetValue5(){
         setCounter5(0)
     }
+   
+    const [counterChanged, setCounterChanged] = useState(0)
+    const [counter5Changed, setcounter5Changed] = useState(0)
 
+    useEffect( ()=> {
+        setCounterChanged(counterChanged + 1)
+    }, [counter])
+    
+    useEffect( ()=> {
+        setcounter5Changed(counter5Changed + 1)
+    }, [counter5])
 
     return (
         <>
             <section className="card">
                 <p>Utilize esta aba para aumentar ou diminuir o valor em 1 unidade:</p>
                 <h2>{counter}</h2>
+                <span>Número de vezes que você já alterou o valor:  {counterChanged - 1} </span><br />
                 <button type="button" onClick={resetValue1} >Zerar</button>
                 <button type="button" onClick={incrementOne} >+1</button>
                 <button type="button" onClick={decrementOne} >-1</button>
@@ -41,6 +52,7 @@ export function Counter(){
             <section className="card">
                 <p>Utilize esta aba para aumentar ou diminuir o valor em 5 unidades:</p>
                 <h2>{counter5}</h2>
+                <span>Número de vezes que você já alterou o valor:  {counter5Changed -1} </span><br />
                 <button type="button" onClick={resetValue5} >Zerar</button>
                 <button type="button" onClick={incrementFive} >+5</button>
                 <button type="button" onClick={decrementFive} >-5</button>
